0b94d1ed7622dccddec82dac2c3ca0d37bb40b0e
dtvangogh <dt.van16@gmail.com>
2020-09-29 13:56:16 -0400

    add

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run me!
+
+sudo apt-get install pip
+sudo pip install beautifulsoup4
+sudo pip install lxml
+sudo pip install requests
+
+echo "All set!"
+echo "Visit the README.md for examples, github links and more"
