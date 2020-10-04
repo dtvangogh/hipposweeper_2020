73061557b5f44c7c6311dff14a239b5b729673f9
Ubuntu <ubuntu@gc-1661-web-01-1599229165.c.hbtn-students-instances.internal>
2020-10-04 01:58:40 +0000

    add

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 # Run me!
 
-sudo apt-get install python3.7
-sudo apt-get install pip
-sudo pip install beautifulsoup4
-sudo pip install lxml
-sudo pip install requests
+sudo apt-get install python
+sudo apt-get install python3-pip
+sudo pip3 install beautifulsoup4
+sudo pip3 install requests
+sudo pip3 install html5lib
+sudo pip3 install lxml
 
 echo "All set!"
 echo "Visit the README.md for examples, github links and more"
